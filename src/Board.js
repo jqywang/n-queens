8310612c22d59570e179198b9fef0aff5f3da8e1
Jeffrey Wang <jqywang.95@gmail.com>
2018-01-24 12:30:52 -0800

    Queens now show conflict going left-right and up-down.

--- src/Board.js
+++ src/Board.js
@@ -8,6 +8,7 @@
 
     initialize: function (params) {
       if (_.isUndefined(params) || _.isNull(params)) {
+      	debugger;
         console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
         console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
         console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
@@ -79,12 +80,23 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+    	var counter = 0;
+    	for(var i = 0; i < this.rows()[rowIndex].length; i++) {
+    		if (this.rows()[rowIndex][i] === 1) {
+    			counter ++;
+    		}
+    	}
+    	return counter > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      for(var i = 0; i < this.rows().length; i++) {
+      	if (this.hasRowConflictAt(i) === true) {
+      		return true;
+      	}
+      }
+      return false;
     },
 
 
@@ -94,12 +106,23 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+    	var counter = 0;
+    	for(var i = 0; i < this.rows().length; i++) {
+    		if (this.rows()[i][colIndex] === 1) {
+    			counter ++;
+    		}
+    	}
+    	return counter > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+    	for (var i = 0; i < this.rows().length; i++) {
+    		if (this.hasColConflictAt(i)) {
+    			return true;
+    		}
+    	}
+    	return false;
     },
 
 
@@ -109,12 +132,29 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    	//checks for major diagonal conflicts at certain indexes provided either row or column is 0
+    	var counter = 0;
+    	// for (var i = 0; i < this.rows().length; i++) { //rows index
+    	// 	for(var j = majorDiagonalColumnIndexAtFirstRow; j < this.rows().length; j++) { //columns index = j
+    	// 		if(this.rows()[i][j] === 1) {
+    	// 			counter ++;
+    	// 		}
+
+    	// 	}
+    	// 	if(counter > 1) {
+    	// 		return true;
+    	// 	} else {
+    	// 		counter = 0;
+    	// 	}
+    	// }
+    	return counter > 1; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+
+    	return false;
+    // fixme
     },
 
 
